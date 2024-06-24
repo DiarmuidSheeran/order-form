f9d29e007a41ec8adaa4a24f2c293d4db69944c5
Diarmuid Sheeran <dssheeran@outlook.com>
2024-06-24 14:14:00 +0000

    Create neccasary apps, set up template folders and test index page works

--- order/settings.py
+++ order/settings.py
@@ -27,8 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = False
 ALLOWED_HOSTS = ['8000-diarmuidsheer-orderform-kxnelb93iua.ws-eu114.gitpod.io', 'order-forms-1ea078c2a2ee.herokuapp.com']
 
 
@@ -44,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'orderForm',
+    'tayto',
+    'walkers',
 ]
 
 MIDDLEWARE = [
--- orderForm/views.py
+++ orderForm/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """
+    Renders the index page.
+    """
+    return render(request, 'orderForm/index.html')
