b794644061b68117e441ea75c75c0a0e94fe1dfe
Diarmuid Sheeran <dssheeran@outlook.com>
2024-06-27 13:24:06 +0000

    Add category model to sort products by category and updated the admin file to reflect changes

--- tayto/admin.py
+++ tayto/admin.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from .models import taytoProduct
+from .models import taytoProduct, Category
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
 
 class taytoProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'productCode', 'quantity', 'weight', 'barcode', 'caseCount')
-    search_fields = ('name', 'productCode', 'barcode')
-    list_filter = ('quantity', 'weight', 'caseCount')
+    list_display = ('name', 'category', 'productCode', 'quantity', 'weight', 'barcode', 'caseCount')
+    search_fields = ('name', 'productCode', 'barcode', 'category__name')  # Updated to allow search by category name
+    list_filter = ('category', 'quantity', 'weight', 'caseCount')
     fieldsets = (
         ('Product Information', {
-            'fields': ('name', 'productCode', 'barcode')
+            'fields': ('name', 'productCode', 'barcode', 'category')  # Added category here
         }),
         ('Stock Information', {
             'fields': ('quantity', 'weight', 'caseCount')
@@ -15,3 +18,4 @@ class taytoProductAdmin(admin.ModelAdmin):
     )
 
 admin.site.register(taytoProduct, taytoProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- tayto/models.py
+++ tayto/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
 
 class taytoProduct(models.Model):
     name = models.CharField(max_length=254)
@@ -9,6 +18,7 @@ class taytoProduct(models.Model):
     weight = models.IntegerField(null=True, blank=True)
     barcode = models.CharField(null=True, blank=True, max_length=254)
     caseCount = models.IntegerField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', null=True)
 
     def __str__(self):
         return f"{self.name}"
