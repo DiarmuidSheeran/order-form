e30ed7d9f69cd68a56edc61551887c1e78248494
Diarmuid Sheeran <dssheeran@outlook.com>
2024-07-02 16:38:37 +0000

    Create the model and edit the admin file for walkers order

--- orderForm/admin.py
+++ orderForm/admin.py
@@ -1,11 +1,17 @@
 # admin.py
 from django.contrib import admin
-from .models import taytoOrder, OrderProduct
+from .models import taytoOrder, OrderProduct, walkersOrder, WalkersOrderProduct
 
 class OrderProductInline(admin.TabularInline):
     model = OrderProduct
     extra = 1
     readonly_fields = ('product', 'quantity')
+
+class WalkersOrderProductInline(admin.TabularInline):
+    model = WalkersOrderProduct
+    extra = 1
+    readonly_fields = ('product', 'quantity')
+
 
 class taytoOrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'display_products')
@@ -16,6 +22,15 @@ class taytoOrderAdmin(admin.ModelAdmin):
         return ", ".join([f"{item.product.name} (Qty: {item.quantity})" for item in obj.orderproduct_set.all()])
     display_products.short_description = 'Products'
 
-admin.site.register(taytoOrder, taytoOrderAdmin)
+class walkersOrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'display_products')
+    search_fields = ('id',)
+    inlines = [WalkersOrderProductInline]
 
+    def display_products(self, obj):
+        return ", ".join([f"{item.product.name} (Qty: {item.quantity})" for item in obj.walkersorderproduct_set.all()])
+    display_products.short_description = 'Products'
+
+admin.site.register(taytoOrder, taytoOrderAdmin)
+admin.site.register(walkersOrder, walkersOrderAdmin)
 
--- orderForm/models.py
+++ orderForm/models.py
@@ -1,6 +1,7 @@
 # models.py
 from django.db import models
 from tayto.models import taytoProduct
+from walkers.models import walkersProduct
 from django.utils import timezone
 
 class taytoOrder(models.Model):
@@ -17,5 +18,21 @@ class taytoOrder(models.Model):
 class OrderProduct(models.Model):
     order = models.ForeignKey(taytoOrder, on_delete=models.CASCADE)
     product = models.ForeignKey(taytoProduct, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+class walkersOrder(models.Model):
+    products = models.ManyToManyField(walkersProduct, through='walkersOrderProduct', related_name='orders')
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"Order {self.pk}"
+
+    def display_products(self):
+        return ", ".join([f"{item.product.name} (Qty: {item.quantity})" for item in self.walkersorderproduct_set.all()])
+    display_products.short_description = 'Products'
+
+class WalkersOrderProduct(models.Model):
+    order = models.ForeignKey(walkersOrder, on_delete=models.CASCADE)
+    product = models.ForeignKey(walkersProduct, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
 
