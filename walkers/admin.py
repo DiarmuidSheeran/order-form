f983d16fdd1ba00d77f33aaa5562add2625d4391
Diarmuid Sheeran <dssheeran@outlook.com>
2024-07-02 11:22:41 +0000

    Create walkers category and product model and design admin.py file to show models in panel

--- walkers/admin.py
+++ walkers/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import walkersProduct, Category
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+class walkersProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'productCode', 'quantity', 'weight', 'barcode', 'caseCount')
+    search_fields = ('name', 'productCode', 'barcode', 'category__name')  # Updated to allow search by category name
+    list_filter = ('category', 'quantity', 'weight', 'caseCount')
+    fieldsets = (
+        ('Product Information', {
+            'fields': ('name', 'productCode', 'barcode', 'category')  # Added category here
+        }),
+        ('Stock Information', {
+            'fields': ('quantity', 'weight', 'caseCount')
+        }),
+    )
+
+admin.site.register(walkersProduct, walkersProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- walkers/models.py
+++ walkers/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+class walkersProduct(models.Model):
+    name = models.CharField(max_length=254)
+    quantity = models.IntegerField(null=True, blank=True)
+    productCode = models.CharField(null=True, blank=True, max_length=254)
+    weight = models.IntegerField(null=True, blank=True)
+    barcode = models.CharField(null=True, blank=True, max_length=254)
+    caseCount = models.IntegerField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Walkers Product"
+        verbose_name_plural = "Walkers Products"
