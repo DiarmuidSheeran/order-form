7bb20742b313fec9c8cfc6dcf026ec96f4cd6084
Diarmuid Sheeran <dssheeran@outlook.com>
2024-07-02 16:45:19 +0000

    Add walkers order form to forms.py file

--- orderForm/forms.py
+++ orderForm/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from tayto.models import taytoProduct
-from .models import taytoOrder
+from walkers.models import walkersProduct
+from .models import taytoOrder, walkersOrder
 
 class ProductQuantityForm(forms.ModelForm):
     quantity = forms.IntegerField(min_value=0, required=True)
@@ -15,6 +16,24 @@ class ProductQuantityForm(forms.ModelForm):
 
 ProductQuantityFormSet = forms.modelformset_factory(
     taytoProduct,
+    form=ProductQuantityForm,
+    extra=0,
+    can_delete=False
+)
+
+class WalkersProductQuantityForm(forms.ModelForm):
+    quantity = forms.IntegerField(min_value=0, required=True)
+
+    class Meta:
+        model = walkersProduct
+        fields = ['id', 'name', 'quantity']
+        widgets = {
+            'name': forms.HiddenInput(),
+            'id': forms.HiddenInput(),  # Ensure id is treated as a hidden field
+        }
+
+ProductQuantityFormSet = forms.modelformset_factory(
+    walkersProduct,
     form=ProductQuantityForm,
     extra=0,
     can_delete=False
